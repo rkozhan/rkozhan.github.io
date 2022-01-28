987c1578347a7722882ca85b41a4304dc7502f84
rkozhan <yalta.vrk@gmail.com>
2022-01-28 13:28:07 +0200

    next-step

--- js/preload copy.js
+++ js/preload copy.js
@@ -4,6 +4,7 @@ const matrixStars = '1234567890-=_`!@#$%^&*()[]{}()[]{}()[]{}()[]{}"/?.>,<;:QWER
 
 window.onload = function () {
 	const preload = document.querySelector('._preload');
+
 	preload.classList.add('_loaded');
 
 	/*
@@ -14,25 +15,29 @@ window.onload = function () {
 	}, 800);
 	*/
 	function makeStar() {
+
 		const matrixStar = document.createElement('div');
 		const matrix = preload.querySelector('.matrix');
 		matrix.append(matrixStar);
 		matrixStar.textContent = matrixStars[getR(0, matrixStars.length)];
 		matrixStar.classList.add('matrix__star');
-		matrixStar.style.transform = `scale(0) rotate(${getR(1, 36) / 10}deg)`;
-		matrixStar.style.color = `rgb(${getR(50, 255)}, ${getR(50, 255)}, ${getR(50, 255)})`;
+
 		//-----------------------------------------------------------event hover
 		matrixStar.addEventListener('mouseenter', (e) => {
 			showTarget(e.target);
+
 		});
 
 		matrixStar.addEventListener('mouseleave', (e) => {
 			showHitTargets(hitCountArray);
+
 		});
 
-		//-------------------------------------------------------------add style
+		//-------------------------------------------------------------
 		setTimeout(() => {
 			const direction = getR(1, 6);
+			const rotation = getR(0, 1);
+
 			if (direction === 1) {
 				matrixStar.style.top = `${getR(0, 50)}%`;
 				matrixStar.style.left = '-30%';
@@ -44,19 +49,19 @@ window.onload = function () {
 				matrixStar.style.left = `${getR(0, 100)}%`;
 			}
 			//--------------------------------rotation
-			let rotation = '';
-			if (getR(0, 1)) rotation = '-';
-			matrixStar.style.transform = `scale(${getR(3, 16)}) rotate(${rotation}${getR(15, 180)}deg)`;
-
+			if (rotation === 0) {
+				matrixStar.style.transform = `scale(${getR(3, 16)}) rotate(${getR(0, 180)}deg)`;
+			} else {
+				matrixStar.style.transform = `scale(${getR(3, 16)}) rotate(-${getR(0, 180)}deg)`;
+			}
+			matrixStar.style.color = `rgb(${getR(0, 255)}, ${getR(0, 255)}, ${getR(0, 255)})`;
 		}, 0);
 
 		setTimeout(() => {
 			matrixStar.remove();
 		}, 10000);
-	} //----------------------------------------------------------------------end Make Star
-
+	}
 	let intervalID = setInterval(makeStar, getR(50, 100));
-
 	//------------------------------------------------------------------------audio
 	const audio = {
 		enter: new Audio("sounds/hover.mp3"),
@@ -100,18 +105,22 @@ window.onload = function () {
 	left:20px;
 	color: rgb(0,255,255);
 	z-index:100;
+
 	`;
 
 	const hitCountArray = [];
+
 
 	//---------------------------------------------------------target targeted
 
 	function showTarget(target) {
 		const audio = new Audio("sounds/locked4.mp3");
 		audio.play();
+		//audio.locked.play();
 		hitCount.style.opacity = 1;
 		hitCount.innerHTML = `target: <span style="color: ${target.style.color}">${target.innerText}</span>`;
 	}
+
 
 	function showHitTargets(targetsArray) {
 		hitCount.style.opacity = 0.4;
@@ -119,12 +128,11 @@ window.onload = function () {
 			hitCount.innerHTML = `${targetsArray.length} hit: ${targetsArray.reverse().join(' ')}`;
 		} else {
 			hitCount.innerHTML = '';
-		}
+		};
 	}
 
 	//-----------------------------------------------------------------shot
-	//document.addEventListener('click', (e) => {
-	preload.addEventListener('click', (e) => {
+	document.addEventListener('click', (e) => {
 		clickToStar(e);
 	});
 
@@ -136,67 +144,79 @@ window.onload = function () {
 				const audio = new Audio("sounds/shot.mp3");
 				audio.volume = 0.1;
 				audio.play();
-			} else if (e.target.closest('.matrix__star')) {
+			}
+			else if (e.target.closest('.matrix__star')) {
+
 				hitCountArray.push(`<span style="color: ${e.target.style.color}">${e.target.innerText}</span>`);
 				showHitTargets(hitCountArray);
 				const audio = new Audio("sounds/smash.mp3");
 				const audio2 = new Audio("sounds/shot.mp3");
 				audio2.play();
 				audio.play();
+				//audio.hit.play();
+				//audio.smash.play();
+
+				let i = 0;
+
 				e.target.style.top = '130%';
 				e.target.style.outline = 'none';
 				let random = '';
 				if (getR(0, 1)) random = '-';
-				e.target.style.zoom = getR(1, 8) / 10;
-				e.target.style.transform = `scale(16) rotateX(${getR(72, 107)}deg) rotateY(${getR(72, 107)}deg) rotateZ(${random}${getR(36, 71)}0deg)`; //rotateX(${getR(72, 107)}deg)
-				e.target.style.transition = `top 1s cubic-bezier(.54,.4,.79,.4), left 3s cubic-bezier(0.58, 0.18, 1, 0.24), transform 1s ease, zoom 1s ease`;
-			};
-		};
+				e.target.style.transform = `scale(16) rotate(${random}${getR(36, 71)}0deg)`; //rotateX(${getR(72, 107)}deg)
+				e.target.style.transition = `top 1s cubic-bezier(.54,.4,.79,.4), left 3s cubic-bezier(0.58, 0.18, 1, 0.24),
+			transform 1s ease`;
+
+				e.target.addEventListener("transitionend", () => {
+					//console.log('end');
+				});
+			}
+		}
 	}
+}
 
-	function laserShot(x, y) {
-		const laser = document.createElement('div');
-		const matrix = document.querySelector('.matrix');
-		const rad = Math.atan((x - (window.innerWidth / 2)) / (window.innerHeight - y));
+function laserShot(x, y) {
+	const laser = document.createElement('div');
+	const rad = Math.atan((x - (window.innerWidth / 2)) / (window.innerHeight - y));
 
-		document.body.prepend(laser);
-		laser.style = `position: fixed;
-	transform: rotate(${rad}rad);
-	transform-origin: top;
-	top: 98%;
-	left: 50%;
-	background-color: rgba(0, 255, 255, 0.5);
-	z-index: 100;
-	width: 10px;
-	height: 200px;
-	transition: top 0.05s linear, left 0.05s linear;`;
-		setTimeout(() => {
-			laser.style.top = y + 'px';
-			laser.style.left = x + 'px';
-		}, 50);
+	document.body.prepend(laser);
+	laser.style = `
+position:fixed;
+transform: rotate(${rad}rad);
+transform-origin: top;
+top:98%;
+left:50%;
+background-color: rgba(0,255,255,0.5);
+z-index:100;
+width:10px;
+height:200px;
+//border-radius: 50%;
+transition: top 0.05s linear, left 0.05s linear;
+`;
+	setTimeout(() => {
+		laser.style.top = y + 'px';
+		laser.style.left = x + 'px';
+	}, 50);
+	laser.addEventListener("transitionend", () => {
+		laser.remove();
+	});
+}
 
-		laser.addEventListener("transitionend", () => {
-			laser.remove();
-		});
-	}
-
-	function getR(min, max) {
-		min = Math.ceil(min);
-		max = Math.floor(max);
-		return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-	}
-
-
-
-	function fragmentate(element) {
-		const text = Array.from(element.textContent);
-		element.textContent = '';
-		text.forEach(symbol => {
-			const span = document.createElement('span');
-			span.innerText = symbol;
-			element.append(span);
-		});
-	}
+function getR(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
 
 
-} ///-------------------------------------------------------------------------end  onload
+
+function fragmentate(element) {
+	const text = Array.from(element.textContent);
+	element.textContent = '';
+	text.forEach(symbol => {
+		const span = document.createElement('span');
+		span.innerText = symbol;
+		element.append(span);
+	});
+
+}
+
--- js/swiper.js
+++ js/swiper.js
@@ -41,8 +41,15 @@ let mainSlider = new Swiper(`${sliderParent} .swiper`, {
 	//HTML:
 	//     <div class="swiper-pagination"></div>
 
+
+	pagination: {
+		el: `${sliderParent} .swiper-pagination`,
+		clickable: true,
+		//type: 'progressbar',
+	},
+
 	/*pagination: {
-		// ---------------------указать элемент 
+	// ---------------------указать элемент 
 		//el: `${sliderParent} .swiper-pagination`,
 		
 		// ИЛИ bullets-------------------------
@@ -86,7 +93,7 @@ let mainSlider = new Swiper(`${sliderParent} .swiper`, {
 	/*
 	scrollbar: {
 		el: `${sliderParent} .swiper-scrollbar`,
-		draggable: true
+		draggable: false
 	},
 	*/
 
@@ -213,202 +220,9 @@ let mainSlider = new Swiper(`${sliderParent} .swiper`, {
 
 
 
-//Инициализация (для нескольких слайдеров указать два селектора, первый как идентификатор):
-sliderParent = '.new-slider';
-let newSlider = new Swiper(`${sliderParent} .swiper`, {
-	// ОПЦИОНАЛЬНЫЕ ПАРАМЕТРЫ:
 
-	// Направление:
-	//direction: 'vertical',
 
-	// Бесконечная прокрутка (скроллбар отключать, мультирядность отключать):
-	loop: true,
-
-	// Количество дублирующих слайдов:
-	//loopedSlides: 0,
-
-	// Свободная прокрутка:
-	//freeMode: true,
-
-	//Автоматическая прокрутка (пауза, остановить на последнем, отключить при ручном):
-	autoplay: {
-		delay: 2000,
-		stopOnLastSlide: false,
-		disableOnInteraction: false
-	},
-
-	// ЕСЛИ нужна pagination (точки):
-	//HTML:
-	//     <div class="swiper-pagination"></div>
-
-	/*pagination: {
-		// ---------------------указать элемент 
-		//el: `${sliderParent} .swiper-pagination`,
-		
-		// ИЛИ bullets-------------------------
-		//type: 'bullets',
-		//clickable: true,
-
-		// динамические bullets----------------
-		//dynamicBullets: true,
-
-		// кастомные bullets--------------------
-		//renderBullet: function (index, className) {
-		//	return `<span class="${className}">${index + 1}</span>`;
-		//},
-		
-		// ИЛИ fractions	------------------------
-		// type: 'fraction',
-
-		// -----------------Кастомные:
-		//renderFraction: function (currentClass, totalClass) {
-		//return `Photo <span class="${currentClass}""></span> from <span class="${totalClass}"></span>`;
-		//},
-	
-		// ИЛИ пргрессбар:
-		//type: 'progressbar',
-	
-	},*/
-
-	// ------------------------------------------------Навигационные кнопки-стрелки
-	//HTML:
-	//     <div class="swiper-button-prev"></div>
-	//     <div class="swiper-button-next"></div>
-	/*
-	navigation: {
-		prevEl: `${sliderParent} .swiper-button-prev`,
-		nextEl: `${sliderParent} .swiper-button-next`,
-	},
-	*/
-	//----------------------------------------------- Если нужен скроллбар
-	//HTML 
-	//    <div class="swiper-scrollbar"></div>)
-	/*
-	scrollbar: {
-		el: `${sliderParent} .swiper-scrollbar`,
-		draggable: true
-	},
-	*/
-
-	// ---------------------------------------------Курсор-рука:
-	//grabCursor: true,
-
-	// ---------------------------------------------Переключение на слайд по которому клик:
-	// slideToClickedSlide: true,
-
-	// ---------------------------------------------Добавление адреса в адресной строке слайду:
-	/*
-	hashNavigation: {
-		watchState: true,
-	},
-	*/
-
-	// ---------------------------------------------Пееключение стрелками:
-	/*
-	keyboard: {
-		enabled: true,
-		onlyInViewport: true,
-	},
-	*/
-
-	// --------------------------------------------Прокрутка колесиком:
-	/*
-	mousewheel: {
-		eventsTarget: `${sliderParent} .swiper`
-	},
-	*/
-
-	//-------------------------------------------- АВТОВЫСОТА (удобно с разным количеством текста):
-	//autoHeight: true,
-
-	// --------------------------------------------ОТСТУП между слайдами:
-	//spaceBetween: 20,
-
-	// --------------------------------------------КОЛИЧЕСТВО показанных слайдов (можно дробное число и auto):
-	//slidesPerView: 1.2,
-
-	// --------------------------------------------КОЛИЧЕСТВО пролистываемых слайдов:
-	//slidesPerGroup: 3,
-
-	// --------------------------------------------Первый активный слайд по центру:
-	//centeredSlides: true,
-
-	// --------------------------------------------Стартовый слайд:
-	//initialSlide: 1,
-
-	//---------------------------------------------Мультирядность (обязательно отключить автовысоту,|
-	//-------------------------------------------------------------количество показанных слайдов 2 или больше)
-	// СSS:
-	//		.slider {
-	//			height: 700px;
-	//			.swiper-slide {
-	//				height: calc((100% - 30px) / 2);
-	//				overflow: hidden;
-	//			}
-	//		}
-	//SlidesPerColumn: 2,
-
-	// -------------------------------------------СКОРОСТЬ переключения (300 по умолчанию):
-	//speed: 1000,
-
-	// -------------------------------------------ЭФФЕКТЫ переключения: (slide по умолчанию):
-	/*
-		effect: 'fade',
-		fadeEffect: {
-			crossFade: true
-		},
-	*/
-
-	/*
-	effect: 'flip',
-	flipEffect: {
-		slideShadows: true,
-		limitRotation: true
-	},
-	*/
-
-	// Для куба ---- в стилях ограничить ширину слайдера 
-	//	.slider { width: 500px;}
-
-	/*
-		effect: 'cube',
-		cubeEffect: {
-			slideShadows: true,
-			shadow: true,
-			shadowOffset: 20,
-			shadowsScale: 0.94
-		},
-	*/
-
-	/*
-	effect: 'coverflow',
-	coverflowEffect: {
-		rotate: 40,
-		stretch: 50,
-		slideShadows: true,
-	},
-	*/
-
-	// --------------------------------------------АДАПТИВ:
-	//	------------------------------Ширина экрана:
-	/*
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-		},
-		480: {
-			slidesPerView: 2,
-		},
-		992: {
-			slidesPerView: 3,
-		},
-	},
-	*/
-
-},
-);
-
-slideOnHover(sliderParent, newSlider);
+//slideOnHover(sliderParent, mainSlider);
 
 
 // Остановка автопрокрутки при наведении: -указать арументы вызова функции для отдельных слайдеров
